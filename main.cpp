596f7e8d4badcf25a82661953d40a31caa8620f7
kolik29 <30481034+kolik29@users.noreply.github.com>
2018-11-06 16:56:34 +0300

    1

--- main.cpp
+++ main.cpp
@@ -6,248 +6,85 @@ using namespace std;
 class BST {
 	public: struct TreeNode {
 		int key; //ключ узла дерева
-		string val = ""; //значение узла2
-		TreeNode *left, *right; //левый и правый подузел
+		TreeNode *left, *right, *parent; //левый, правый и родительский подузлы
 	};
+	
+	public: TreeNode* search(TreeNode *&node, int k) { //поиск
+		if ((node == NULL) || (k == node->key)) {
+			return node;
+		}
 		
-	public: int treeSize(TreeNode *Tree) {
-		if (Tree->left == NULL && Tree->right == NULL)
-			return 1;
-
-		int left, right;
-
-		if (Tree->left != NULL)
-			left = treeSize(Tree->left);
-		else
-			left = 0;
-		if (Tree->right != NULL)
-			right = treeSize(Tree->right); 
-		else 
-			right = 0;
-			
-		return left+right+1;
-	}
-
-	public: void showAll(TreeNode *&Tree) { //показывает все узлы
-		if (Tree != NULL) { //если дерево не пустое
-			showAll(Tree->left); //рекурсивно вызывает левое поддерево
-			cout << "Номер узла: " << Tree->key << ". Значение: " << Tree->val; //получает значение текущего узла
-			if (Tree->left != NULL) //если левое поддерево не пустое
-				cout << ". Левый узел: " << Tree->left->key; //то выводит номер левого подузла
-			if (Tree->right != NULL) //аналогично для правого
-				cout << ". Правый узел: " << Tree->right->key;
-			cout << endl;
-			showAll(Tree->right); //рекурсивно вызывает правое поддерево
+		if (k < node->key) {
+			return search(node->left, k);
+		} else {
+			return search(node->right, k);
 		}
 	}
+	
+	public: TreeNode* ins(TreeNode *&node, int k) { //вставка
+		if (node == NULL) {
+			node->key = k;
+			return node;
+		} else if (k < node->key) {
+			node->left = ins(node->left, k);
+		} else if (k > node->key) {
+			node->right = ins(node->right, k);
+		}
+	}
+	
+	public: TreeNode* del(TreeNode *&node, int k) { //удаление
+		if (node == NULL) {
+			return node;
+		}
 		
-	public: void showNode(int key, TreeNode *&Tree) { //показывает узел
-		if (Tree != NULL) { //если дерево не пустое
-			showNode(key, Tree->left); //рекурсивно вызывает левое поддерево
-			if (key == Tree->key) {
-				cout << "Номер узла: " << Tree->key << ". Значение: " << Tree->val; //получает значение текущего узла
-				if (Tree->left != NULL) //если левое поддерево не пустое
-					cout << ". Левый узел: " << Tree->left->key; //то выводит номер левого подузла
-				if (Tree->right != NULL) //аналогично для правого
-					cout << ". Правый узел: " << Tree->right->key;
-				cout << endl;
-			}
-			showNode(key, Tree->right); //рекурсивно вызывает правое поддерево
-		}
-	}
-
-	public: void addTreeNode(int key, string val, TreeNode *&Tree) { //добавление нового узла
-		if (NULL == Tree) { //если дерево пустое
-			Tree = new TreeNode; //выделяется память
-			Tree->key = key; //добавляется ключ
-			Tree->val = val; //значение
-			Tree->left = Tree->right = NULL; //левый и правый узел
-		}
-
-		if (key < Tree->key) { //если указанное значение ключа меньше чем значение узла
-			if (Tree->left != NULL) { //если левый узел не пустой
-				addTreeNode(key, val, Tree->left); //рекурсивно вызывается добавление левого узла
-			} else { //иначе
-				Tree->left = new TreeNode; //выделяется память
-				Tree->left->left = Tree->left->right = NULL; //добавляется левый и правый подузел
-				Tree->left->key = key; //ключ
-				Tree->left->val = val; //и значение узла
-			}
-		}
-
-		if (key > Tree->key) { //аналагично для правого подузла
-			if (Tree->right != NULL) {
-				addTreeNode(key, val, Tree->right);
+		if (k < node->key) {
+			node->left = del(node->left, k);
+		} else if (k > node->key) {
+			node->right = del(node->right, k);
+		} else if ((node->left != NULL) && (node->right != NULL)) {
+			node->key = min(node->right)->key;
+			node->right = del(node->right, node->key);
+		} else {
+			if (node->left != NULL) {
+				node = node->left;
 			} else {
-				Tree->right = new TreeNode;
-				Tree->right->left = Tree->right->right = NULL;
-				Tree->right->key = key;
-				Tree->right->val = val;
+				node = node->right;
 			}
 		}
-				
-		if (key == Tree->key) {
-			Tree->val = val;
-		}
-	}
-
-	public: void clear(TreeNode *&Tree) { //очистка дерева
-		if (Tree != NULL) { //если дерево не пустое
-			clear(Tree->left); //рекурсивный вызывает левое поддерево
-			clear(Tree->right); //...правое...
-			delete Tree; //очищает память
-			Tree = NULL; //очищает дерево
-		}
+		return node;
 	}
 	
-	public: TreeNode* minNode(TreeNode *&Tree) {
-		if (Tree->left == NULL)
-			return Tree;
-		return minNode(Tree->left);
-	}
-	
-	public: TreeNode* deleteNode(TreeNode *&Tree, int key) { //показывает узел
-		if (Tree != NULL) { //если дерево не пустое
-			if (key < Tree->key) {
-				Tree->left = deleteNode(Tree->left, key);
-			} else if (key > Tree-> key) {
-				Tree->right = deleteNode(Tree->right, key);
-			} else if ((Tree->left != NULL) && (Tree->right != NULL)) {
-				Tree->key = minNode(Tree->right)->key;
-				Tree->right = deleteNode(Tree->right, Tree->key);
-			} else {
-				if (Tree->left != NULL)
-					Tree = Tree->left;
-				else
-					Tree = Tree->right;
-			}
+	public: TreeNode* min(TreeNode *&node) { //наименьшее
+		if (node->left == NULL) {
+			return node;
 		}
-		return Tree;
-	}
-	
-	public: TreeNode* next(TreeNode *&Tree, int x) {
-		TreeNode* current = Tree;
-		TreeNode* successor = NULL;
-		while (current != NULL) {
-			if (current->key > x) {
-				successor = current;
-				current = current->left;
-			} else
-				current = current->right;
-		}
-		return successor;
-	}
-	
-	public: TreeNode* prev(TreeNode *&Tree, int x) {
-		TreeNode* current = Tree;
-		TreeNode* successor = NULL;
-		while (current != NULL) {
-			if (current->key < x) {
-				successor = current;
-				current = current->left;
-			} else
-				current = current->right;
-		}
-		return successor;
-	}
 		
-	public: void treeMenu(TreeNode *&Tree) {
-		while(true) {
-			int command, key;
-			string val;
-			cout << "Команды:\n0 - выйти из программы,\n1 - показать все узлы дерева,\n2 - очистить дерево,\n3 - добавить узел,\n4 - показать определенный узел;\n5 - размер дерева;\n6 - удалить узел;\n7 - перейти к итератеру.\n:> ";
-			cin >> command;
-			switch (command) {
-				case 0:
-					exit(1);
-					break;
-			
-				case 1:
-					showAll(Tree);
-					break;
-
-				case 2:
-					clear(Tree);
-					break;
-					
-				case 3:
-					cout << "Введите значение ключа: ";
-					cin >> key;
-					cout << "Введите значение узла: ";
-					cin >> val;
-					addTreeNode(key, val, Tree);
-					cout << "Узел " << key << " успешно добавлен.\n";
-					break;  
-					
-				case 4:
-					cout << "Введите значение ключа: ";
-					cin >> key;
-					showNode(key, Tree);
-					break;
-					
-				case 5:
-					cout << "Разамер дерева: " << treeSize(Tree) << "\n";
-					break;
-					
-				case 6:
-					cout << "Введите значение ключа: ";
-					cin >> key;
-					Tree = deleteNode(Tree, key);
-					break;
-										
-				case 7:
-					iteratorMenu(Tree);
-					break;
-			}
-		}
+		return min(node->right);
 	}
 	
-	public: void iteratorMenu(TreeNode *&Tree) {
-		string command, help;
-		int currentKey = Tree->key;
-		
-		help = "root - установка на корень.\nend - проверка конца дерева.\ninfo - информация об элементе.\nnext - переход к следующему элементу.\nprev - переход к предыдущему элементу.\nmenu - вернуться в главное меню.\nhelp - показать эту справку.\n";
-		cout << help;
-		
-		while (true) {
-			cout << "<" << currentKey << ">: ";
-			cin >> command;
-						
-			if (command == "root")
-				currentKey = Tree->key;
-			
-			if (command == "end") {
-				if ((Tree->left == NULL) && (Tree->right == NULL))
-					cout << "Конец дерева.\n";
-				else
-					cout << "Не конец дерева.\n";
-			}
-			
-			if (command == "next")
-				currentKey = next(Tree, currentKey)->key;
-			
-			if (command == "prev")
-				currentKey = prev(Tree, currentKey)->key;
-			
-			if (command == "info")
-				showNode(currentKey, Tree);
-					
-			if (command == "menu")
-				treeMenu(Tree);
+	public: TreeNode* max(TreeNode *&node) { //наибольшее
+		if (node->right == NULL) {
+			return node;
 		}
+		
+		return max(node->right);
+	}
+	
+	public: int traversal(TreeNode *&node) {
+		if (node != NULL) {
+			traversal(node->left);
+			traversal(node->right);
+			cout << node->key << " ";
+		}
+		
+		return NULL;
+	}
+	
+	public: int count(TreeNode *&node) {
+		
 	}
 };
 
 int main() {
-	BST binaryTree;
-	BST::TreeNode *BSTree = NULL; //выделение памяти для дерева
-	srand(time(0)); //рандом, зависящий от времени
-
-	for (int i = 0; i < 10; i++) { //цикл от 0 до 100
-		binaryTree.addTreeNode(1 + rand() % 100, "val_" + to_string(i), BSTree); //добавление узла в дерево
-	}
-
-	binaryTree.treeMenu(BSTree);
-		
-	return 0;
+    
 }
